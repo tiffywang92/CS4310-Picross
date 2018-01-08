c3764cafc848853b0fd297471dc9ba332afcd132
scgtam <35149773+scgtam@users.noreply.github.com>
2018-01-08 11:03:54 -0800

    Update Picross_1.java
    Removing Sai's code

--- Picross_1/src/picross_1/Picross_1.java
+++ Picross_1/src/picross_1/Picross_1.java
@@ -15,38 +15,7 @@ public class Picross_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[][] userGrid = new int[4][4];
-        int [][] pcGrid = new int [4][4];
-        createGrid (pcGrid);
-        drawGrid(pcGrid);
         System.out.println("Sample");
-        
     }
 
-    public static int [][] createGrid(int [][] grid) {
-    	Random r = new Random(); 
-    	for (int y = 0 ; y < 4 ; y++)
-    		for ( int x = 0 ; x < 4 ; x++)
-    			if (r.nextBoolean())
-    				grid[x][y] = 1;
-    	return grid;
-    }
-
-    public static void drawGrid(int grid[][]) {
-        for (int i = 0; i <4; i++) {
-            System.out.print(' ');
-            System.out.print('_');
-        }
-        System.out.println(' ');
-        for (int y = 0; y < 4; y ++) {
-            for (int x = 0; x <4; x++) {
-                System.out.print('|');
-                if (grid[x][y] == 1)
-                    System.out.print('O');
-                else
-                    System.out.print('_');
-            }
-        System.out.println('|');
-        }
-    }
 }

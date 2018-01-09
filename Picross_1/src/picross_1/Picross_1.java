5a95660d9ba677a8218a1f71265aad81964b5abb
scgtam <35149773+scgtam@users.noreply.github.com>
2018-01-08 16:06:38 -0800

    Update Picross_1.java
    update method createSol and added global variable

--- Picross_1/src/picross_1/Picross_1.java
+++ Picross_1/src/picross_1/Picross_1.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package picross_1;
+import java.util.Random;
+
+
 
 /**
  *
@@ -11,45 +14,68 @@ package picross_1;
  */
 public class Picross_1 {
 
+    static boolean [][]solGrid = new boolean[4][4];
+    static int [][] trueX  = new int [4][2];
+    static int [][] trueY = new int [4][2];
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Sample Puzzle\n");
-        int[][] userGrid = new int[4][4];   // to do: print out blank grid
-        int [][] pcGrid = new int [4][4];   // to match with solution
-        createGrid (pcGrid);
-        drawGrid(pcGrid);
+    	//grid first = new grid();
+    	//first.setGrid(createGrid());
+    	createSol();
+    	drawGrid();
+    	
+        //userGrid[0][1]= 1;
+        //createGrid (pcGrid);
+        //drawGrid(pcGrid);
+        System.out.println("Sample");   
     }
 
-    /* createGrid: generates solution grid */
-    public static int [][] createGrid(int [][] grid) {
-        Random r = new Random(); 
-        for (int y = 0 ; y < 4 ; y++)
-            for ( int x = 0 ; x < 4 ; x++)
-                if (r.nextBoolean())
-                    grid[x][y] = 1;
-        return grid;
+    static boolean [][]solGrid = new boolean[4][4];
+    static int [][] trueX  = new int [4][2];
+    static int [][] trueY = new int [4][2];
+    
+	/*Create and initialize the solution grid randomly, also calculate the random grid hint and store in trueX and trueY*/
+    public static void createSol() {
+    	
+    	Random r = new Random(); 
+    	for (int y = 0 ; y < 4 ; y++)
+    		for ( int x = 0 ; x < 4 ; x++) 
+    			solGrid[x][y] = r.nextBoolean();
+    	
+    	//Compute x-axis hint
+    	int tempCount = 0;
+    	for (int y = 0 ; y < 4 ; y++) {
+    		for ( int x = 0 ; x < 4 ; x++) {
+    			if (solGrid[y][x] == true)
+    				trueX[y][tempCount] ++;		//Found box true, up the hint count
+    			else if (trueX[y][0] !=0 && solGrid[y][x] == false)
+    				tempCount  = 1; 	// Found box false and previous is not true, move to 2nd element
+    			else 
+    				tempCount = tempCount;  //box false, do nothing
+    		}
+    	tempCount = 0;   
+    	}	
+    	
+    	tempCount = 0;  //reset count for picking up y axis hint
+    	
+    	//Compute x-axis hint
+    	for ( int x = 0 ; x < 4 ; x++) {
+    		for (int y = 0 ; y < 4 ; y++) {
+    			if (solGrid[y][x] == true)
+    				trueY[x][tempCount] ++;		//Found box true, up the hint count
+    			else if (trueY[x][0] !=0 && solGrid[y][x] == false)
+    				tempCount  = 1; 	// Found box false and previous is not true, move to 2nd element
+    			else 
+    				tempCount = tempCount;	//box false, do nothing
+    		}
+    	tempCount = 0;
+    	}	
     }
+    
  
     /* drawGrid: prints out solution grid */
-    public static void drawGrid(int grid[][]) {
-        for (int i = 0; i <4; i++) {
-            System.out.print(' ');
-            System.out.print('_');
-        }
-        System.out.println(' ');
-        for (int y = 0; y < 4; y ++) {
-            for (int x = 0; x <4; x++) {
-                System.out.print('|');
-                if (grid[x][y] == 1)
-                    System.out.print('O');
-                else
-                    System.out.print('_');
-            }
-        System.out.println('|');
-        }
-    }
 }
 
 /** Insert another method here to generate user grid + number clues

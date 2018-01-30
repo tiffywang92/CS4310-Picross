774deb1fc43eaf6020057b330bba1a6efe6ccc3b
anthonyEShay <35188695+anthonyEShay@users.noreply.github.com>
2018-01-29 22:19:10 -0800

    Error Fixes
    Fixed errors and made sure everything worked with the other classes.

--- HW2/src/main/PicrossModel.java
+++ HW2/src/main/PicrossModel.java
@@ -2,7 +2,6 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Random;
-import GameBoard.java;
 
 /**
  * The Model component manages the system data and associated operations on that data.
@@ -14,83 +13,97 @@ public class PicrossModel {
 	
 	private GameBoard solutionB;
 	private GameBoard userB;
-	private ArrayList<ArrayList<Integer>> trueX;
-	private ArrayList<ArrayList<Integer>> trueY;
+	private Integer modelSize;
+	private ArrayList<ArrayList<Integer>> rowHints;
+	private ArrayList<ArrayList<Integer>> columnHints;
 	
 	/**
 	 * Creates a random game board and stores it in solutionB.
 	 * @param gameSize - the size of the 2d square game board
 	 */
 	private void generateSolution(int gameSize) {
-		// Check for grid size
-		if (gameSize < 4)
-			gameSize = 4;	// set minimum grid size; cannot be less than 4 for gameplay purposes
-					// i.e. a grid size of 2 or 3 makes too small a square for an array of hints
-					// a size of 1 or less doesn't make a grid
 		
 		// Create randomnized grid
 		Random r = new Random(); 
 		for (int y = 0 ; y < gameSize ; y++)
 			for ( int x = 0 ; x < gameSize ; x++) {
 				if (r.nextInt(2) == 1)
-					solutionB[x][y] = 'o';
+					solutionB.setSquare(x, y, 'o');
 				else
-					solutionB[x][y] = ' ';
+					solutionB.setSquare(x, y, ' ');
 			}
 
 		//  Reset User Grid
 		for (int y = 0 ; y < gameSize ; y++)
 			for ( int x = 0 ; x < gameSize ; x++) 
-				userB[x][y] = ' ';   
+				userB.setSquare(x, y, ' ');
 	}
 	
 	/**
 	 * Generates the hints for solutionB and stores them in trueX and trueY.
 	 */
 	private void generateTrueXY(int gameSize) {
-		// Check for grid size
-		if (gameSize < 4)
-			gameSize = 4;
 		
-		// Reset hint grid to 0
-		for (int y = 0 ; y < gameSize ; y++)
-			if (gameSize % 2 == 0)		//If size is even
-				for ( int x = 0 ; x < gameSize / 2 ; x++) {
-					trueX[y][x] = 0;
-					trueY[y][x] = 0;
-			else				//If size is odd
-				for ( int x = 0 ; x < gameSize / 2 + 1 ; x++) {
-					trueX[y][x] = 0;
-					trueY[y][x] = 0;
-			}
-		}
+//		// Reset hint grid to 0
+//		for (int y = 0 ; y < gameSize ; y++)
+//			if (gameSize % 2 == 0)		//If size is even
+//				for ( int x = 0 ; x < gameSize / 2 ; x++) {
+//					trueX[y][x] = 0;
+//					trueY[y][x] = 0;
+//			else				//If size is odd
+//				for ( int x = 0 ; x < gameSize / 2 + 1 ; x++) {
+//					trueX[y][x] = 0;
+//					trueY[y][x] = 0;
+//			}
+//		}
 
-		//Compute x-axis hint
+		//Compute row hints
 		int tempCount = 0;
-		for (int y = 0 ; y < gameSize ; y++) {
-		    for ( int x = 0 ; x < gameSize ; x++) {
-			if (solutionB[y][x] == 1)
-			    trueX[y][tempCount] ++;		//Found box true, up the hint count
-			else if (trueX[y][0] !=0 && solutionB[y][x] == 0)
-			    tempCount  = 1; 	// Found box false and previous is not true, move to 2nd element
-			else;  //box false, do nothing
+		for (int x = 0 ; x < gameSize ; x++) {
+			rowHints.add(new ArrayList<Integer>());
+		    for ( int y = 0 ; y < gameSize ; y++) {
+		    	if(solutionB.getBoardValue(x, y) == 'o') {
+		    		++tempCount;	//Found box true, up the hint count
+		    	}else {
+		    		if(tempCount != 0) {
+		    			rowHints.get(x).add(tempCount);
+		    		}
+		    		tempCount = 0;
+		    	}
 		    }
-		    tempCount = 0;
-		}	
+		    if(tempCount == 0 && rowHints.get(x).size() == 0) {
+		    	rowHints.get(x).add(tempCount);
+		    }
+		    if(tempCount != 0) {
+		    	rowHints.get(x).add(tempCount);
+		    	tempCount = 0;
+		    }
+		}
 
 		tempCount = 0;  //reset count for picking up y-axis hint
 
-		//Compute y-axis hint
-		for ( int x = 0 ; x < gameSize ; x++) {
-		    for (int y = 0 ; y < gameSize ; y++) {
-			if (solutionB[y][x] == 1)
-			    trueY[x][tempCount] ++;		//Found box true, up the hint count
-			else if (trueY[x][0] !=0 && solutionB[y][x] == 0)
-			    tempCount  = 1; 	// Found box false and previous is not true, move to 2nd element
-			else;	//box false, do nothing
+		//Compute column hints
+		for ( int y = 0 ; y < gameSize ; y++) {
+			columnHints.add(new ArrayList<Integer>());
+		    for (int x = 0 ; x < gameSize ; x++) {
+		    	if(solutionB.getBoardValue(x, y) == 'o') {
+		    		++tempCount;	//Found box true, up the hint count
+		    	} else {
+		    		if(tempCount != 0) {
+		    			columnHints.get(y).add(tempCount);
+		    		}
+		    		tempCount = 0;
+		    	}
 		    }
-		    tempCount = 0;
+		    if(tempCount == 0 && columnHints.get(y).size() == 0) {
+		    	columnHints.get(y).add(tempCount);
+		    }
+		    if(tempCount != 0) {
+		    	columnHints.get(y).add(tempCount);
+		    	tempCount = 0;
+		    }
 		}
+		
 	}
 	
 	/**
@@ -98,10 +111,21 @@ public class PicrossModel {
 	 * @param gameSize - the size to make the 2d square game board
 	 */
 	public PicrossModel(Integer gameSize) {
-		generateSolution(gameSize);	// Load solution grid
-		generateTrueXY(gameSize);	// Load hint grid
-		if (!isCorrect)
-			correct = true;		// Set correct flag to be 'pulled' by PicrossView.java
+		// Check for grid size
+		// set minimum grid size; cannot be less than 4 for gameplay purposes, more than 10 is unwieldy
+		// i.e. a grid size of 2 or 3 makes too small a square for an array of hints
+		// a size of 1 or less doesn't make a grid
+		Integer gSize = gameSize;
+		if (gSize < 4 || gSize > 10) {
+			gSize = 4;
+		}
+		modelSize = gSize;
+		solutionB = new GameBoard(gSize);
+		userB = new GameBoard(gSize);
+		rowHints = new ArrayList<ArrayList<Integer>>();
+		columnHints = new ArrayList<ArrayList<Integer>>();
+		generateSolution(gSize);	// Load solution grid
+		generateTrueXY(gSize);	// Load hint grid
 	}
 	
 	/**
@@ -110,21 +134,29 @@ public class PicrossModel {
 	 * @param y
 	 * @param value
 	 */
-	public void setSquareUserBoard(Integer x, Integer y, Integer value) {
+	public void setSquareUserBoard(Integer x, Integer y, char value) {
 		userB.setSquare(x, y, value);
 	}
 	
+	public char getSquareUserBoard(Integer x, Integer y) {
+		return userB.getBoardValue(x, y);
+	}
+	
+	public char getSquareSolBoard(Integer x, Integer y) {
+		return solutionB.getBoardValue(x, y);
+	}
+	
 	/**
-	 * This returns a copy of either the X column hints or Y column hints.
-	 * @param xOrY - 0 for the X column hints, anything else for Y column hints
+	 * This returns a copy of either the column or row hints.
+	 * @param cOrR - 0 for the column hints, anything else for row hints
 	 * @return
 	 */
-	public ArrayList<ArrayList<Integer>> getHints(Integer xOrY){
+	public ArrayList<ArrayList<Integer>> getHints(Integer cOrR){
 		//Given that trueX and trueY have been initialized, return the respective array
-		if(xOrY == 0) {
-			return trueX;
+		if(cOrR == 0) {
+			return columnHints;
 		}else {
-			return trueY;
+			return rowHints;
 		}
 	}
 	
@@ -144,22 +176,25 @@ public class PicrossModel {
 		return userB;
 	}
 	
+	public Integer getModelSize() {
+		return modelSize;
+	}
+	
 	/**
 	 * Checks and compares the solution to the current user board.
 	 * @return - true if the user's board matches the solution, false otherwise.
 	 */
 	public boolean isCorrect() {
-		boolean correct = true;  //correct flag
 
-		for (int i = 0; i<solutionB.length; i++){
-		    for (int j = 0; j<solutionB[i].length; j++){
-			if(solutionB[i][j] != userB[i][j]){
-			    correct = false;
-			}
+		for (int i = 0; i < solutionB.getSize(); i++){
+		    for (int j = 0; j < solutionB.getSize(); j++){
+		    	if(solutionB.getBoardValue(i, j) != userB.getBoardValue(i, j)){
+			    return false;
+			    }
 		    }
 		}
 
-		return correct;
+		return true;
 	}
 	
 }

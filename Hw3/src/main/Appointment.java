c5c9fe26a8d2adc625749d72a42d7fee060c4328
anthonyEShay <35188695+anthonyEShay@users.noreply.github.com>
2018-02-12 18:00:55 -0800

    Skeleton

---
+++ Hw3/src/main/Appointment.java
@@ -0,0 +1,51 @@
+package main;
+
+/**
+ * Creates an immutable object that stores all values needed for an appointment
+ *
+ */
+public class Appointment {
+
+	private Integer year, month, day, hour, minute;
+	private String title, description;
+	
+	public Integer getYear() {
+		return year;
+	}
+
+	public Integer getMonth() {
+		return month;
+	}
+
+	public Integer getDay() {
+		return day;
+	}
+
+	public Integer getHour() {
+		return hour;
+	}
+
+	public Integer getMinute() {
+		return minute;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	Appointment(Integer year, Integer month, Integer day, Integer hour, Integer minute, String title, String description){
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.hour = hour;
+		this.minute = minute;
+		this.title = title;
+		this.description = description;
+	}
+	
+	
+}
